b455e1c92ca47a587230d2f0c43514b0bda867ae
Thalutn5 <37041254+Thalutn5@users.noreply.github.com>
2020-03-02 14:23:30 +0700

    update

--- Tugas2/udp_simple.py
+++ Tugas2/udp_simple.py
@@ -1,1 +1,8 @@
-test
+
+import socket
+
+TARGET_IP = "10.151.62.1"
+TARGET_PORT = 5005
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.sendto(bytes('Tugas 2 Program Jaringan'.encode()),(TARGET_IP,TARGET_PORT))
